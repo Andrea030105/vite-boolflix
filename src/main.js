99170b534fafaca2c6fae220df6542c60d50ecf3
Andrea030105 <tummoloandrea05@gmail.com>
2026-01-30 16:15:34 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+// Importa CSS di Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Importa JavaScript di Bootstrap (opzionale, solo se usi componenti interattivi)
+import * as bootstrap from "bootstrap/dist/js/bootstrap.bundle";
+
+const app = createApp(App);
+app.provide("bootstrap", bootstrap);
+app.mount("#app");
